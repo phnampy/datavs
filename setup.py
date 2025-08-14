d52e7d4c30067bd8e57434d5ad9480a11895d234
Hồ Phương Nam <phnampy@gmail.com>
2025-08-14 10:25:40 +0700

    Cập nhật phiên bản trong setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='datavs',
-    version='1.0',
+    version='1.0.1',
     package_dir={"": "src"},
     packages=find_packages(where="src"),
     install_requires=["pandas", "geopandas", "matplotlib", "seaborn"],
